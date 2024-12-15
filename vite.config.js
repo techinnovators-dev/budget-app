10801cb7124fbc71d75be862b44070cb1a083965
Daniel Guidry <daniel@techinnovators.dev>
2024-12-15 12:26:30 -0500

    test serviceworker registration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import vuetify from 'vite-plugin-vuetify'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +12,10 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    vuetify()
+    vuetify(),
+    VitePWA({
+      injectRegister: 'auto'
+    })
   ],
   resolve: {
     alias: {
